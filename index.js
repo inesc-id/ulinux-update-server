7c79ecb9278511af80c9cb797382f57d18c192ae
Tomás Pinho <me@tomaspinho.com>
2016-02-04 20:56:55 +0000

    starting work on serving image files, adding database pool to project

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 'use strict';
 
 const Hapi = require('hapi');
+const Inert = require('inert');
 const config = require(__dirname + '/config.js');
 const fs = require('fs');
+const Path = require('path');
 
 const routes = require('./routes');
+
+const mysql = require('mysql');
+const db  = mysql.createPool({
+  connectionLimit : 10,
+  host            : config.db_host,
+  database        : config.db_database,
+  user            : config.db_user,
+  password        : config.db_pass,
+});
 
 const options = {
   key: fs.readFileSync(config.key_path),
@@ -15,9 +26,16 @@ const server = new Hapi.Server();
 server.connection({
   port: config.port,
   tls: options,
+  routes: {
+    files: {
+      relativeTo: Path.join(__dirname, 'public'),
+    },
+  },
 });
 
-server.route(routes(config));
+server.register(Inert, () => {});
+
+server.route(routes(config, db));
 
 server.start((err) => {
   console.log('uLinux Update Server running at:', server.info.uri);
--- routes/images.js
+++ routes/images.js
@@ -1,3 +1,60 @@
+const Boom = require('boom');
+
 module.exports = function (config, db) {
-  return [];
+
+  const getImage = {
+    method: 'GET',
+    path: '/images/{id}',
+    handler: function (request, reply) {
+      db.query(
+        'select * from device_images where id = ?',
+        [request.params.id],
+        (err, result) => {
+
+          if (err) {
+
+            const response = Boom.badImplementation(
+              'Error found while fetching device image ' +
+              'information from the database',
+              err
+            );
+            reply(response);
+
+          } else {
+
+            reply.file('device_images/' + result.filename);
+          }
+        }
+      );
+    },
+  };
+
+  const getLatestImageMetadata = {
+    method: 'GET',
+    path: '/images/latest',
+    handler: function (request, reply) {
+      db.query(
+        'select * from device_images order by timestamp desc',
+        [request.params.id],
+        (err, result) => {
+
+          if (err) {
+
+            const response = Boom.badImplementation(
+              'Error found while fetching latest image metadata ' +
+              'information from the database',
+              err
+            );
+            reply(response);
+
+          } else {
+
+            reply(result[0]);
+          }
+        }
+      );
+    },
+  }
+
+  return [].concat(getImage, getLatestImageMetadata);
 };
