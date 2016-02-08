4d657bac524c178047015837d4e055674010940c
Tomás Pinho <me@tomaspinho.com>
2016-02-08 16:01:49 +0000

    creating utilities folder. creating sql database bootstrapping utility and schema

---
+++ db/schema.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS `device_images` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `filename` text NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `device_imalives` (
+  `device_id` text NOT NULL,
+  `firmware_version` int(11) NOT NULL,
+  `ip` varchar(39) NOT NULL,
+  `port` int(11) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
