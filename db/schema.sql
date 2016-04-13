aa21bd7754c16ef3d618adeb12eb8d7efdc7a3e5
Tomás Pinho <me@tomaspinho.com>
2016-04-13 19:08:25 +0100

    correct ipv6 max length is 45

--- db/schema.sql
+++ db/schema.sql
@@ -8,7 +8,7 @@ CREATE TABLE IF NOT EXISTS `device_images` (
 CREATE TABLE IF NOT EXISTS `device_imalives` (
   `device_id` text NOT NULL,
   `firmware_version` int(11) NOT NULL,
-  `ip` varchar(39) NOT NULL,
+  `ip` varchar(45) NOT NULL,
   `port` int(11) NOT NULL,
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
